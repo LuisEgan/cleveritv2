ea5440967c47444ced8cf10f486f90a8c314e627
Carlos Gallardo Arenas <cgallardo@cleverit.cl>
2019-09-27 18:35:37 -0300

    so far

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Layout, SEO } from 'Common'
 import { Intro, Skills, Contact } from 'Components/landing'
+import usePosts from '../hooks/use-posts'
+import PostPreview from '../components/blog/post-preview'
 
-export default () => (
-	<Layout>
-		<SEO />
-		<Intro />
-		<Skills />
-		<Contact />
-	</Layout>
-)
+export default () => {
+	const posts = usePosts()
+	console.log('TCL: posts', posts)
+	return (
+		<Layout>
+			<SEO />
+			<Intro />
+			<Skills />
+			<h2> Material para digerir</h2>
+			{posts.map(post => (
+				<PostPreview key={post.slug} post={post} />
+			))}
+			<Contact />
+		</Layout>
+	)
+}
